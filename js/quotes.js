c5f8390c6de9c955b0026ed75834899481bf6a3e
Antonio Riccelli <anton.riccelli@gmail.com>
2021-09-24 20:55:07 +0100

    add pics and quotes

---
+++ js/quotes.js
@@ -0,0 +1,92 @@
+const quotes = [
+    {
+        "id": 1,
+        "author": "Marcus Aurelius",
+        "quote": "You have control over your mind - not outside events. Realize this, and you will find strength."
+    },
+    {
+        "id": 2,
+        "author": "Titus Maccius Plautus",
+        "quote": "If you have overcome your inclination and not been overcome by it, you have reason to rejoice."
+    },
+    {
+        "id": 3,
+        "author": "Augustus",
+        "quote": "I found Rome a city of bricks and left it the city of marbles."
+    },
+    {
+        "id": 4,
+        "author": "Julius Caesar",
+        "quote": "Which death is preferable to every other? The unexpected."
+    },
+    {
+        "id": 5,
+        "author": "Tullius Cicero",
+        "quote": "Advice in old age is foolish; for what can be more absurd than to increase our provisions for the road the nearer we approach our journey’s end."
+    },
+    {
+        "id": 6,
+        "author": "Quintus Horatius Flaccus",
+        "quote": "A shoe that is too large is apt to trip one, and when too small, to pinch the feet. So it is with those whose fortune does not suit them."
+    },
+    {
+        "id": 7,
+        "author": "Scipio Africanus",
+        "quote": "I’m never less at leisure than when at leisure, or less alone than when alone."
+    },
+    {
+        "id": 8,
+        "author": "Publilius Syrus",
+        "quote": "An angry man is again angry with himself when he returns to reason."
+    },
+    {
+        "id": 9,
+        "author": "Titus Livius",
+        "quote": "Rome has grown since its humble beginnings that it is now overwhelmed by its own greatness."
+    },
+    {
+        "id": 10,
+        "author": "Gaius Sallustius Crispus",
+        "quote": "Those most moved to tears by every word of a preacher are generally weak and a rascal when the feelings evaporate."
+    },
+    {
+        "id": 11,
+        "author": "Marcus Tullius Cicero",
+        "quote": "If you have a garden and a library, you have everything you need."
+    },
+    {
+        "id": 12,
+        "author": "Publius Ovidius Naso",
+        "quote": "There is no such thing as pure pleasure; some anxiety always goes with it."
+    },
+    {
+        "id": 13,
+        "author": "Tiberius Claudius Caesar Augustus Germanicus",
+        "quote": "Say not always what you know, but always know what you say."
+    },
+    {
+        "id": 14,
+        "author": "Gaius Plinius Secundus",
+        "quote": "Hope is the pillar that holds up the world. Hope is the dream of a waking man."
+    },
+    {
+        "id": 15,
+        "author": "Marcus Annaeus Lucanus",
+        "quote": "The gods conceal from men the happiness of death, that they may endure life."
+    },
+    {
+        "id": 16,
+        "author": "Aulus Gellius",
+        "quote": "Another one of the old poets, whose name has escaped my memory at present, called Truth the daughter of Time"
+    },
+    {
+        "id": 17,
+        "author": "Publius Flavius Vegetius Renatus",
+        "quote": "We find that the Romans owed the conquest of the world to no other cause than continual military training, exact observance of discipline in their camps, and unwearied cultivation of the other arts of war."
+    },
+    {
+        "id": 18,
+        "author": "Phaedrus",
+        "quote": "Witticisms please as long as we keep them within boundaries, but pushed to excess they cause offense."
+    }
+]
