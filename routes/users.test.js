b55a134505c2b5e037f4665cd4923abe7f27f7fa
Antonio Riccelli <anton.riccelli@gmail.com>
2021-12-29 18:24:38 +0000

    Updated HTML and styling

--- routes/users.test.js
+++ routes/users.test.js
@@ -1,32 +1,40 @@
 import app from "../app";
 import request from "supertest";
 
-
-
 describe("checking get /users response", function () {
-    test("testing get request to /users ", async function () {
-      //arrange
-      //act
-      //assert
-      await request(app)
-        .get("/users")
-        .expect(200)
-        .expect(function (res) {
-          const actual = res.body;
-          const expected = {
-            success: true,
-            message: expect.any(String),
-            payload: expect.any(Array),
-          };
-          expect(actual).toStrictEqual(expected);
-          actual.payload.forEach((quote) =>
-            expect(quote).toStrictEqual({
-              id: expect.any(Number),
-              author: expect.any(String),
-              quote: expect.any(String)
-            })
-          );
-        });
-    });
-})
+  test("testing get request to /users ", async function () {
+    //arrange
+    //act
+    //assert
+    await request(app)
+      .get("/users")
+      .expect(200)
+      .expect(function (res) {
+        const actual = res.body;
+        const expected = {
+          success: true,
+          message: expect.any(String),
+          payload: expect.any(Array),
+        };
+        expect(actual).toStrictEqual(expected);
+      });
+  });
 
+  test("testing get request to /users/4", async function () {
+    await request(app)
+      .get("/users/4")
+      .expect(200)
+      .expect(async function (res) {
+        const actual = await res.body;
+        const expected = {
+          success: true,
+          payload: {
+            id: expect.any(Number),
+            message: expect.any(String),
+            payload: expect.any(Array)
+          },
+        };
+        expect(actual).toStrictEqual(expected);
+      });
+  });
+});
