1fa0cf0614e28a19676a344441fb3614dddb63c1
Antonio Riccelli <anton.riccelli@gmail.com>
2021-12-30 19:12:46 +0000

    added search by ID and a basic search by name feature

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,7 @@ import convertToRoman from "./conversion.js";
 // NUMBER CONVERSION CODE [start]
 
 // VALIDATE USER INPUT
-const validate = function(input) {
+const validate = function (input) {
     if (typeof input === 'number' && input >= 1 && input <= 5999) {
         console.log(`This is a valid input: ${input}. Data type: ${typeof input}.`)
         return input;
@@ -13,21 +13,21 @@ const validate = function(input) {
         console.error(error);
         return error;
     }
-} 
-   
+}
+
 // ADD ROMAN NUMERAL TO HTML
 function outputRomanNumeral(roman) {
     let par = document.getElementById("output");
-    par.classList.remove("bg-danger", "text-white", "error-message")   
+    par.classList.remove("bg-danger", "text-white", "error-message")
     par.innerText = roman;
-    par.classList.add("border-bottom","border-dark","border-2" );
+    par.classList.add("border-bottom", "border-dark", "border-2");
 }
 
 // DISPLAY ERROR
 function outputError(error) {
     let par = document.getElementById("output");
     par.innerText = error.message;
-    par.classList.add("border-bottom","border-dark","border-2", "bg-danger", "text-white", "error-message");
+    par.classList.add("border-bottom", "border-dark", "border-2", "bg-danger", "text-white", "error-message");
     throw error;
 }
 
@@ -37,20 +37,28 @@ function playAudio() {
 }
 
 // DISPLAY ERROR POPUP
+// FIRST, RESET IT, SO THAT THE POPUP REAPPEARS EVERY TIME YOU INPUT THE WRONG FORMAT
+
+
 function displayPopup() {
     let popupDiv = document.querySelector(".popup");
     let popup = document.querySelector("#justinian");
     popup.style.display = "inline-block";
     popup.style.visibility = "visible";
-    popup.style.animation = "fadeIn 2s";
+    popup.style.animation = "fadeIn 4s linear forwards";
     popupDiv.style.display = "inline-block";
     popupDiv.style.visibility = "visible";
-    popupDiv.style.animation = "fadeIn 2s";
+    popupDiv.style.animation = "fadeIn 4s linear forwards";
+    popup.classList.toggle("noneHidden");
+    popupDiv.classList.toggle("noneHidden");
+    popup.classList.toggle("fadeInOut");
+    popupDiv.classList.toggle("fadeInOut");
     console.log("Function executed")
+
 }
 
 // GET USER INPUT
-const getUserInput = function(ev) {
+const getUserInput = function (ev) {
     let num = document.querySelector("form").elements[0].value;
     num = Number(num);
     console.log(`This is the current user input: ${num}.\nIts data type is ${typeof num}.`)
@@ -59,22 +67,21 @@ const getUserInput = function(ev) {
 
 // THE FUNCTION TO EXECUTE ALL FUNCTIONS
 
-function convertAndDisplay(){
-let userInput = getUserInput();
-console.log(`This is what userInput returned: ${userInput}. Data type: ${typeof userInput}.`)
-let validatedInput = validate(userInput);
-if (typeof validatedInput === 'object') {
-    displayPopup();
-    playAudio();
-    outputError(validatedInput);
-
-}
-let convertedNumber = convertToRoman(validatedInput);
-outputRomanNumeral(convertedNumber);
+function convertAndDisplay() {
+    let userInput = getUserInput();
+    console.log(`This is what userInput returned: ${userInput}. Data type: ${typeof userInput}.`)
+    let validatedInput = validate(userInput);
+    if (typeof validatedInput === 'object') {
+        displayPopup();
+        playAudio();
+        outputError(validatedInput);
+    }
+    let convertedNumber = convertToRoman(validatedInput);
+    outputRomanNumeral(convertedNumber);
 }
 
 // GET ELEMENT AND ADD CLICK EVENT LISTENER
-const init = function() {
+const init = function () {
     document.getElementById("button-send").addEventListener("click", convertAndDisplay);
 }
 
@@ -112,14 +119,17 @@ function getRandomInt(max) {
 
 // UPDATE QUOTE AND RELATED DOM ELEMENTS
 async function updateQuote() {
-let ind = getRandomInt(quotes.length);
-let authorName = document.getElementById("author");
-let quoteElement = document.querySelector("q");
-let authorPic = document.getElementById("author-picture");
-// console.log(quotes);
-authorPic.setAttribute("src", `./images/authors/${quotes[ind].id}-${quotes[ind].author.toLowerCase().split(" ").join("-")}.jpg`)
-authorName.innerText = quotes[ind].author;
-quoteElement.innerText = quotes[ind].quote;
+    let ind = getRandomInt(quotes.length);
+    let authorName = document.getElementById("author");
+    let quoteElement = document.querySelector("q");
+    let container = document.getElementById("quotewrapper");
+    container.classList.add("p-5")
+    authorName.classList.toggle("p-2");
+    let authorPic = document.getElementById("author-picture");
+    authorPic.setAttribute("src", `./images/authors/${quotes[ind].id}-${quotes[ind].author.toLowerCase().split(" ").join("-")}.jpg`)
+    authorName.innerText = quotes[ind].author;
+    quoteElement.innerText = quotes[ind].quote;
+
 }
 
 // INVOKE FUNCTIONS
@@ -129,27 +139,73 @@ setInterval(updateQuote, 12000)
 
 // QUOTE GENERATOR CODE [end]
 
-
 // RETRIEVE ALL QUOTES
 
-const getQuotesUponUserRequest = () => {
-    const idInput = document.getElementById("quote-id").value;
 
-if (idInput === 0 || !idInput) {
-    quotes.forEach(quote => {
-        const quotesContainer = document.getElementById("quotes-container");
-        const newQuoteWrapper = document.createElement("div");
-        const authorDiv = document.createElement("div");
-        const quoteDiv = document.createElement("q");
-        authorDiv.innerText = quote.author;
-        authorDiv.classList.toggle("display-6")
-        quoteDiv.innerText = quote.quote;
-        newQuoteWrapper.appendChild(authorDiv);
-        newQuoteWrapper.appendChild(quoteDiv);
-        quotesContainer.appendChild(newQuoteWrapper);
-    })
+
+const getQuotesUponUserRequest = () => {
+    const quotesContainer = document.getElementById("quotes-container");
+    quotesContainer.classList.toggle("p-2", "p-2");
+    // RESET THE QUOTE CONTAINER SO AS TO NOT DUPLICATE QUOTES UPON CLICKING BUTTON
+    quotesContainer.innerHTML = "";
+    const idInput = +document.getElementById("quote-id").value;
+    const nameInput = document.getElementById("authorName").value
+
+    if (idInput === 0 || !idInput) {
+        if (nameInput) {
+            let retrievedQuote = quotes.find(quote => quote.author === nameInput);
+        console.log(retrievedQuote);
+        quotesContainer.classList.toggle("p-1");
+            const newQuoteWrapper = document.createElement("div");
+            newQuoteWrapper.classList.toggle("p-3");
+            newQuoteWrapper.classList.toggle("m-4");
+            newQuoteWrapper.classList.toggle("shadow");
+            newQuoteWrapper.classList.toggle("rounded-borders");
+            const authorDiv = document.createElement("div");
+            const quoteDiv = document.createElement("q");
+            authorDiv.innerText = retrievedQuote.author;
+            authorDiv.classList.toggle("display-6");
+            quoteDiv.innerText = retrievedQuote.quote;
+            newQuoteWrapper.appendChild(authorDiv);
+            newQuoteWrapper.appendChild(quoteDiv);
+            quotesContainer.appendChild(newQuoteWrapper);
+    } else {
+        quotes.forEach(quote => {
+            const newQuoteWrapper = document.createElement("div");
+            newQuoteWrapper.classList.toggle("p-3");
+            newQuoteWrapper.classList.toggle("m-4");
+            newQuoteWrapper.classList.toggle("shadow");
+            newQuoteWrapper.classList.toggle("rounded-borders");
+            const authorDiv = document.createElement("div");
+            const quoteDiv = document.createElement("q");
+            authorDiv.innerText = quote.author;
+            authorDiv.classList.toggle("display-6");
+            quoteDiv.innerText = quote.quote;
+            newQuoteWrapper.appendChild(authorDiv);
+            newQuoteWrapper.appendChild(quoteDiv);
+            quotesContainer.appendChild(newQuoteWrapper);
+        })};
+    } else {
+        let retrievedQuote = quotes.find(quote => quote.id === idInput);
+        console.log(retrievedQuote);
+        quotesContainer.classList.toggle("p-1");
+            const newQuoteWrapper = document.createElement("div");
+            newQuoteWrapper.classList.toggle("p-3");
+            newQuoteWrapper.classList.toggle("m-4");
+            newQuoteWrapper.classList.toggle("shadow");
+            newQuoteWrapper.classList.toggle("rounded-borders");
+            const authorDiv = document.createElement("div");
+            const quoteDiv = document.createElement("q");
+            authorDiv.innerText = retrievedQuote.author;
+            authorDiv.classList.toggle("display-6");
+            quoteDiv.innerText = retrievedQuote.quote;
+            newQuoteWrapper.appendChild(authorDiv);
+            newQuoteWrapper.appendChild(quoteDiv);
+            quotesContainer.appendChild(newQuoteWrapper);
+    }
 }
-}
+
+
 
 function activateQuoteRequestInput() {
     const quoteButton = document.getElementById("quote-button");
@@ -162,26 +218,26 @@ activateQuoteRequestInput()
 let state = "light";
 
 const toggleMode = () => {
-if (state === "light") {
-    state === "dark";
-    document.body.style.color = "black";
-} else {
-    state === "light";
-    document.body.style.color = "white";
-}
-const source = modePicture.src;
-if (/color/.test(source)) {
-    modePicture.src = "images/sun-warm.svg"
-} else {
-modePicture.setAttribute("src", "images/sun-color.svg")
-}
-let navbar = document.querySelector("nav");
-navbar.classList.toggle("bg-dark");
-navbar.classList.toggle("bg-light");
+    if (state === "light") {
+        state === "dark";
+        document.body.style.color = "black";
+    } else {
+        state === "light";
+        document.body.style.color = "white";
+    }
+    const source = modePicture.src;
+    if (/color/.test(source)) {
+        modePicture.src = "images/sun-warm.svg"
+    } else {
+        modePicture.setAttribute("src", "images/sun-color.svg")
+    }
+    let navbar = document.querySelector("nav");
+    navbar.classList.toggle("bg-dark");
+    navbar.classList.toggle("bg-light");
 }
 
 const modePicture = document.querySelector("#mode");
 modePicture.addEventListener("click", toggleMode);
 
 
-export {getUserInput, convertToRoman, updateQuote, getRandomInt, outputRomanNumeral }
+export { getUserInput, convertToRoman, updateQuote, getRandomInt, outputRomanNumeral }
